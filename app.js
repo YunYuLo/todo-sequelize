e092c3ea03c4f2617a7914eaa126f988686a545e
Vivian Lo <yunyubee@gmail.com>
2019-10-03 13:49:52 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -14,6 +16,20 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'your secret key',
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 //router
 app.get('/', (req, res) => {
