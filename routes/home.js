4240c525e83c7533f3d48afa0a18d207801f1313
Vivian Lo <yunyubee@gmail.com>
2019-10-03 16:26:00 +0800

    add todo CRUD

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,16 @@ const User = db.User
 const Todo = db.Todo
 const { authenticated } = require('../config/auth')
 
-router.get('/', (req, res) => {
-  res.send('hello world')
+router.get('/', authenticated, (req, res) => {
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error("user not found")
+      return Todo.findAll({
+        where: { UserId: req.user.id }
+      })
+    })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
